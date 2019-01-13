a9344684a42aafe7e9be8c2986a8c51b9db51c09
greg <gregfrazier@gmail.com>
2019-01-13 15:33:47 -0500

    added sample java file

---
+++ HelloWorld.java
@@ -0,0 +1,21 @@
+//*******************************************************************
+// Welcome to CompileJava!
+// If you experience any issues, please contact us ('More Info')  -->
+// Also, sorry that the "Paste" feature no longer works! GitHub broke
+// this (so we'll switch to a new provider): https://blog.github.com\
+// /2018-02-18-deprecation-notice-removing-anonymous-gist-creation/
+//*******************************************************************
+
+// one class needs to have a main() method
+public class HelloWorld
+{
+  // arguments are passed using the text field below this editor
+  public static void main(String[] args)
+  {
+    int a = 0;
+    for(a = 0; a < 10; a++) {
+      System.out.println(a);
+    }
+    System.out.println("Complete!");
+  }
+}
